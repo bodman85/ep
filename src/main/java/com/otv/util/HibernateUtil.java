e3236f825d1dd76470c93ebd0e18b0958e36a1bb
bodman85 <bodman@mail.ru>
2014-01-09 00:06:26 +0200

    config files reverted after merge

--- src/main/java/com/otv/util/HibernateUtil.java
+++ src/main/java/com/otv/util/HibernateUtil.java
@@ -15,16 +15,9 @@ public class HibernateUtil {
 
 	public static SessionFactory getSessionFactory() {
 		if (sessionFactory == null) {
-			Configuration cfg = new Configuration();
-			cfg.configure();
-			cfg.setProperty(
-					"hibernate.connection.url", "jdbc:mysql://"+
-					System.getenv("OPENSHIFT_MYSQL_DB_HOST")+":"+
-					System.getenv("OPENSHIFT_MYSQL_DB_PORT")+"/"+
-					System.getenv("OPENSHIFT_APP_NAME"));
-			sessionFactory = cfg.buildSessionFactory();
+			sessionFactory = new Configuration().configure()
+					.buildSessionFactory();
 		}
-
 		return sessionFactory;
 	}
 
