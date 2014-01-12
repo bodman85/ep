63f2295cadcd6e0a394b326ec4290f5fe8829d7f
bodman85 <bodman@mail.ru>
2014-01-12 17:01:56 +0200

    Removing branch inconsistencies by employing maven profiles.

--- src/main/java/com/otv/util/HibernateUtil.java
+++ src/main/java/com/otv/util/HibernateUtil.java
@@ -1,5 +1,6 @@
 package com.otv.util;
 
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -11,17 +12,37 @@ import org.hibernate.cfg.Configuration;
  */
 public class HibernateUtil {
 
-	private static SessionFactory sessionFactory = null;
+	private static SessionFactory sessionFactory;
+	/*	
+	private static ServiceRegistry serviceRegistry;
 
+	public static SessionFactory getSessionFactory() throws HibernateException {
+		if (sessionFactory == null) {
+			Configuration configuration = new Configuration();
+			
+			configuration.setProperty("hibernate.connection.url",
+					"jdbc:mysql://" + System.getenv("OPENSHIFT_MYSQL_DB_HOST")
+							+ ":" + System.getenv("OPENSHIFT_MYSQL_DB_PORT")
+							+ "/" + System.getenv("OPENSHIFT_APP_NAME"));
+			
+			configuration.configure();
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(
+					configuration.getProperties()).buildServiceRegistry();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		}
+		return sessionFactory;
+	}
+*/
+	
 	public static SessionFactory getSessionFactory() {
 		if (sessionFactory == null) {
 			Configuration cfg = new Configuration();
-			cfg.configure();
 			cfg.setProperty(
 					"hibernate.connection.url", "jdbc:mysql://"+
 					System.getenv("OPENSHIFT_MYSQL_DB_HOST")+":"+
 					System.getenv("OPENSHIFT_MYSQL_DB_PORT")+"/"+
 					System.getenv("OPENSHIFT_APP_NAME"));
+			cfg.configure();
 			sessionFactory = cfg.buildSessionFactory();
 		}
 
