9d759d8745fcb1d730a5126017c783c9ff3c237e
bodman85 <bodman@mail.ru>
2014-02-06 22:45:18 +0200

    login/logout fix

--- src/main/java/com/edp/LoginManagedBean.java
+++ src/main/java/com/edp/LoginManagedBean.java
@@ -1,52 +1,69 @@
 package com.edp;
 
+import java.io.IOException;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
-
 import org.primefaces.context.RequestContext;
 
-
-@ManagedBean(name="loginMBean")
+@ManagedBean(name = "loginMBean")
 @SessionScoped
-public class LoginManagedBean {  
-	  
-    private String username;  
-      
-    private String password;  
-      
-    public String getUsername() {  
-        return username;  
-    }  
-  
-    public void setUsername(String username) {  
-        this.username = username;  
-    }  
-  
-    public String getPassword() {  
-        return password;  
-    }  
-  
-    public void setPassword(String password) {  
-        this.password = password;  
-    }  
-  
-    public void login(ActionEvent actionEvent) {  
-        RequestContext context = RequestContext.getCurrentInstance();  
-        FacesMessage msg = null;  
-        boolean loggedIn = false;  
-          
-        if(username != null  && username.equals("admin") && password != null  && password.equals("admin")) {  
-            loggedIn = true;  
-            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);  
-        } else {  
-            loggedIn = false;  
-            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Invalid credentials");  
-        }  
-          
-        FacesContext.getCurrentInstance().addMessage(null, msg);  
-        context.addCallbackParam("loggedIn", loggedIn);  
-    }  
-}  
+public class LoginManagedBean {
+
+	private String username;
+
+	private String password;
+
+	private boolean loggedIn = false;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+
+	public void login(ActionEvent actionEvent) throws IOException{
+	//	RequestContext context = RequestContext.getCurrentInstance();
+		FacesMessage msg = null;
+
+		if (username != null && username.equals("admin") && password != null
+				&& password.equals("admin")) {
+			loggedIn = true;
+		//	msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome",
+		//			username);
+			FacesContext.getCurrentInstance()
+			   .getExternalContext().redirect("/ep/pages/index.xhtml?faces-redirect=true");
+		} else {
+			loggedIn = false;
+			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error",
+					"Invalid credentials");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
+//		context.addCallbackParam("loggedIn", loggedIn);
+		return;
+	}
+
+	public String logout() {
+		FacesContext.getCurrentInstance().getExternalContext()
+				.invalidateSession();
+		loggedIn = false;
+		return "/pages/index.xhtml?faces-redirect=true";
+	}
+}
--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -1,11 +1,22 @@
 	function handleLoginRequest(xhr, status, args) {
-		alert("Hello World!");
 		if (args.validationFailed || !args.loggedIn) {
 			PF('dlg').jq.effect("shake", {
 				times : 5
 				}, 100);
 		} else {
 			PF('dlg').hide();
+			toggle("loginPanelId");
+			toggle("logoutPanelId");
 			$('#loginLink').fadeOut();
 		}
 	}
+
+    function toggle(id) {
+    	//alert("toggle run");
+        var element = document.getElementById(id);
+        if(element.style.display != 'none') {
+          element.style.display = 'none';
+        } else {
+          element.style.display = 'block';
+        }
+    }
