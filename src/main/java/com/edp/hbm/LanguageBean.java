89aa56836803dfab179228c4356bff1af3ca8caa
bodman85 <bodman@mail.ru>
2014-03-15 13:22:19 +0200

    Using browser locale instead of server locale

--- src/main/java/com/edp/hbm/LanguageBean.java
+++ src/main/java/com/edp/hbm/LanguageBean.java
@@ -1,14 +1,11 @@
 package com.edp.hbm;
 
 import java.io.Serializable;
-import java.util.LinkedHashMap;
 import java.util.Locale;
-import java.util.Map;
-
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ValueChangeEvent;
 
 @ManagedBean(name = "languageMBean")
 @SessionScoped
@@ -19,51 +16,28 @@ public class LanguageBean implements Serializable{
 	 */
 	private static final long serialVersionUID = -1400222915795226683L;
 
-	private String localeCode = "ru";
 	private Locale locale;
+	private String language;
 
-	private static Map<String, Object> countries;
-	static {
-		countries = new LinkedHashMap<String, Object>();
-		countries.put("Русский", new Locale("ru"));
-		countries.put("English", Locale.ENGLISH);
-	}
+	@PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+        language = locale.getLanguage(); 
+    }
 
-	public Map<String, Object> getCountriesInMap() {
-		return countries;
-	}
+    public Locale getLocale() {
+        return locale;
+    }
 
-	public String getLocaleCode() {
-		return localeCode;
-	}
+    public String getLanguage() {
+    	language = locale.getLanguage(); 
+        return language;
+    }
 
-	public void setLocaleCode(String localeCode) {
-		this.localeCode = localeCode;
-	}
-	
-	public Locale getLocale() {
-		return locale;
-	}
-
-	public void setLocale(Locale locale) {
-		this.locale = locale;
-	}
-
-	// value change event listener
-	public void countryLocaleCodeChanged(ValueChangeEvent e) {
-
-		String newLocaleCode = e.getNewValue().toString();
-
-		// loop country map to compare the locale code
-		for (Map.Entry<String, Object> entry : countries.entrySet()) {
-
-			if (entry.getValue().toString().equals(newLocaleCode)) {
-				setLocaleCode(newLocaleCode);
-				setLocale((Locale) entry.getValue());
-				FacesContext.getCurrentInstance().getViewRoot()
-						.setLocale((Locale) entry.getValue());
-			}
-		}
-	}
+    public void setLanguage(String language) {
+    	this.language = language;
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
 
 }
